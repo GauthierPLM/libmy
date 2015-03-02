d3b58793d83dd064cc783d72d19b5b69f3db97e1
pogam-_g <pogam-_g@epitech.eu>
2015-03-02 15:40:54 +0100

    Add xfork function

--- src/xfork.c
+++ src/xfork.c
@@ -1,4 +1,16 @@
+/*
+** xfork.c for minishell2 in /home/pogam-_g/dev/C/lib/my/src
+**
+** Made by gauthier pogam-lemontagner
+** Login   <pogam-_g@epitech.net>
+**
+** Started on  Mon Mar  2 15:31:09 2015 gauthier pogam-lemontagner
+** Last update Mon Mar  2 15:31:10 2015 gauthier pogam-lemontagner
+*/
+
 #include <unistd.h>
+#include <libmy/my.h>
+#include <stdlib.h>
 
 int	xfork(void)
 {
@@ -6,6 +18,8 @@ int	xfork(void)
 
   if ((pid = fork()) == -1)
     {
-      
+      my_fprintf(2, "Can’t perform fork");
+      exit(1);
     }
+  return (pid);
 }
