b3363df4c413f161761301918b6f02cc73c2a562
pogam-_g <pogam-_g@epitech.eu>
2015-03-26 17:40:39 +0100

    Manage NULL strings

--- src/my_fusion.c
+++ src/my_fusion.c
@@ -11,28 +11,34 @@
 #include <malloc.h>
 #include <libmy/my.h>
 
+char		*my_fusion_copy(char *from, char *dest)
+{
+  static int	i = 0;
+  int		j;
+
+  j = 0;
+  while (from[j] && dest[i])
+    {
+      dest[i] = from[j];
+      i++;
+      j++;
+    }
+  return (dest);
+}
+
 char	*my_fusion(char *s1, char *s2)
 {
-  int	i;
-  int	j;
-  int	k;
+  int s1_len;
+  int s2_len;
   char	*dest;
 
-  i = my_strlen(s1);
-  j = my_strlen(s2);
-  if ((dest = malloc((i + j + 1) * sizeof(char))) == NULL)
-    return (NULL);
-  k = i;
-  i = 0;
-  while (i < k)
-    {
-      dest[i] = s1[i];
-      i++;
-    }
-  k = j;
-  j = 0;
-  while (j < k)
-    dest[i++] = s2[j++];
-  dest[i] = '\0';
+  s1_len = (s1) ? my_strlen(s1) : 0;
+  s2_len = (s2) ? my_strlen(s2) : 0;
+  dest = xmalloc((s1_len + s2_len + 1) * sizeof(char));
+  if (s1)
+    my_fusion_copy(s1, dest);
+  if (s2)
+    my_fusion_copy(s2, dest);
+  dest[s1_len + s2_len] = '\0';
   return (dest);
 }
